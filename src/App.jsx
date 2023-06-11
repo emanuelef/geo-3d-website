0d3784a308cea430d605ed5285c2b7d81a86c6dd
Emanuele Fumagalli <emanuelef@gmail.com>
2023-06-11 19:54:09 +0100

    adding data points

--- src/App.jsx
+++ src/App.jsx
@@ -5,30 +5,42 @@ import { createRoot } from "react-dom/client";
 import DeckGL from "@deck.gl/react";
 import { LineLayer, ScatterplotLayer } from "@deck.gl/layers";
 
-
 const MAP_STYLE =
   "https://basemaps.cartocdn.com/gl/dark-matter-nolabels-gl-style/style.json";
 
-const INITIAL_VIEW_STATE = {
-  latitude: 51.47,
-  longitude: 0.45,
-  zoom: 4,
-  bearing: 0,
-  pitch: 30,
+const DATA_URL = {
+  POINTS: "./points.json", // eslint-disable-line
 };
 
-function Root() {
-  const onClick = (info) => {
-    if (info.object) {
-      // eslint-disable-next-line
-      alert(
-        `${info.object.properties.name} (${info.object.properties.abbrev})`
-      );
-    }
-  };
+const INITIAL_VIEW_STATE = {
+  longitude: -0.341004,
+  latitude: 51.477487,
+  zoom: 12,
+  minZoom: 10,
+  maxZoom: 18,
+  pitch: 40.5,
+  bearing: -27.396674584323023,
+};
+
+export default function App() {
+  const layers = [
+    new ScatterplotLayer({
+      id: "airports",
+      data: DATA_URL.POINTS,
+      radiusScale: 16,
+      getPosition: (d) => d.coordinates,
+      getFillColor: [255, 0, 255, 100],
+      getRadius: 6,
+      pickable: true,
+    }),
+  ];
 
   return (
-    <DeckGL controller={true} initialViewState={INITIAL_VIEW_STATE}>
+    <DeckGL
+      layers={layers}
+      controller={true}
+      initialViewState={INITIAL_VIEW_STATE}
+    >
       <Map
         reuseMaps
         mapLib={maplibregl}
@@ -40,4 +52,4 @@ function Root() {
 }
 
 const container = document.body.appendChild(document.createElement("div"));
-createRoot(container).render(<Root />);
+createRoot(container).render(<App />);
