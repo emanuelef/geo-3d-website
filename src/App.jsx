d9bbb49459fb336257aabb86d12cfb272e3f9846
Emanuele Fumagalli <emanuelef@gmail.com>
2023-06-11 20:08:21 +0100

    adding sample data

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,12 @@ const MAP_STYLE =
   "https://basemaps.cartocdn.com/gl/dark-matter-nolabels-gl-style/style.json";
 
 const DATA_URL = {
-  POINTS: "./points.json", // eslint-disable-line
+  POINTS:
+    "https://raw.githubusercontent.com/emanuelef/geo-3d-website/main/data/points.json",
+  SEGMENTS:
+    "https://raw.githubusercontent.com/emanuelef/geo-3d-website/main/data/segments.json",
+  MIN_DISTANCE:
+    "https://raw.githubusercontent.com/emanuelef/geo-3d-website/main/data/mindistance.json",
 };
 
 const INITIAL_VIEW_STATE = {
@@ -22,10 +27,20 @@ const INITIAL_VIEW_STATE = {
   bearing: -27.396674584323023,
 };
 
+function getTooltip({ object }) {
+  console.log(object);
+  return (
+    object &&
+    `\
+  ${object.name || ""}
+  ${object.minDistance || ""}`
+  );
+}
+
 export default function App() {
   const layers = [
     new ScatterplotLayer({
-      id: "airports",
+      id: "points",
       data: DATA_URL.POINTS,
       radiusScale: 16,
       getPosition: (d) => d.coordinates,
@@ -33,12 +48,35 @@ export default function App() {
       getRadius: 6,
       pickable: true,
     }),
+    new LineLayer({
+      id: "segments",
+      data: DATA_URL.SEGMENTS,
+      opacity: 0.8,
+      getSourcePosition: (d) => d.start,
+      getTargetPosition: (d) => d.end,
+      getColor: [0, 255, 0, 120],
+      getWidth: 2,
+      pickable: true,
+      //dataTransform: (d) => d.filter((f) => f.icao == FLIGHT_ICAO),
+    }),
+    new LineLayer({
+      id: "min-distance",
+      data: DATA_URL.MIN_DISTANCE,
+      opacity: 0.8,
+      getSourcePosition: (d) => d.start,
+      getTargetPosition: (d) => d.end,
+      getColor: [255, 0, 0, 200],
+      getWidth: 2,
+      pickable: true,
+      //dataTransform: (d) => d.filter((f) => f.icao == FLIGHT_ICAO),
+    }),
   ];
 
   return (
     <DeckGL
       layers={layers}
       controller={true}
+      getTooltip={getTooltip}
       initialViewState={INITIAL_VIEW_STATE}
     >
       <Map
