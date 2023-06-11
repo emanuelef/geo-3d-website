8271d40ca357039f8c8cbe8b9acc28c6a9793355
Emanuele Fumagalli <emanuelef@gmail.com>
2023-06-11 17:42:51 +0100

    working on deck.gl view

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
+import { Map } from "react-map-gl";
+import maplibregl from "maplibre-gl";
 import { createRoot } from "react-dom/client";
-import DeckGL, { GeoJsonLayer, ArcLayer } from "deck.gl";
+import DeckGL from "@deck.gl/react";
+import { LineLayer, ScatterplotLayer } from "@deck.gl/layers";
 
-// source: Natural Earth http://www.naturalearthdata.com/ via geojson.xyz
-const COUNTRIES =
-  "https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_50m_admin_0_scale_rank.geojson"; //eslint-disable-line
-const AIR_PORTS =
-  "https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_10m_airports.geojson";
+
+const MAP_STYLE =
+  "https://basemaps.cartocdn.com/gl/dark-matter-nolabels-gl-style/style.json";
 
 const INITIAL_VIEW_STATE = {
   latitude: 51.47,
@@ -28,44 +29,15 @@ function Root() {
 
   return (
     <DeckGL controller={true} initialViewState={INITIAL_VIEW_STATE}>
-      <GeoJsonLayer
-        id="base-map"
-        data={COUNTRIES}
-        stroked={true}
-        filled={true}
-        lineWidthMinPixels={2}
-        opacity={0.4}
-        getLineColor={[60, 60, 60]}
-        getFillColor={[200, 200, 200]}
-      />
-      <GeoJsonLayer
-        id="airports"
-        data={AIR_PORTS}
-        filled={true}
-        pointRadiusMinPixels={2}
-        pointRadiusScale={2000}
-        getPointRadius={(f) => 11 - f.properties.scalerank}
-        getFillColor={[200, 0, 80, 180]}
-        pickable={true}
-        autoHighlight={true}
-        onClick={onClick}
-      />
-      <ArcLayer
-        id="arcs"
-        data={AIR_PORTS}
-        dataTransform={(d) =>
-          d.features.filter((f) => f.properties.scalerank < 4)
-        }
-        getSourcePosition={(f) => [-0.4531566, 51.4709959]}
-        getTargetPosition={(f) => f.geometry.coordinates}
-        getSourceColor={[0, 128, 200]}
-        getTargetColor={[200, 0, 80]}
-        getWidth={1}
+      <Map
+        reuseMaps
+        mapLib={maplibregl}
+        mapStyle={MAP_STYLE}
+        preventStyleDiffing={true}
       />
     </DeckGL>
   );
 }
 
-/* global document */
 const container = document.body.appendChild(document.createElement("div"));
 createRoot(container).render(<Root />);
