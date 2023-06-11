1704b4609c9152b342adc62012a79b92eb584e4c
Emanuele Fumagalli <emanuelef@gmail.com>
2023-06-11 17:29:57 +0100

    building Deck.gl view

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,71 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import DeckGL, { GeoJsonLayer, ArcLayer } from "deck.gl";
 
-function App() {
-  const [count, setCount] = useState(0)
+// source: Natural Earth http://www.naturalearthdata.com/ via geojson.xyz
+const COUNTRIES =
+  "https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_50m_admin_0_scale_rank.geojson"; //eslint-disable-line
+const AIR_PORTS =
+  "https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_10m_airports.geojson";
+
+const INITIAL_VIEW_STATE = {
+  latitude: 51.47,
+  longitude: 0.45,
+  zoom: 4,
+  bearing: 0,
+  pitch: 30,
+};
+
+function Root() {
+  const onClick = (info) => {
+    if (info.object) {
+      // eslint-disable-next-line
+      alert(
+        `${info.object.properties.name} (${info.object.properties.abbrev})`
+      );
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <DeckGL controller={true} initialViewState={INITIAL_VIEW_STATE}>
+      <GeoJsonLayer
+        id="base-map"
+        data={COUNTRIES}
+        stroked={true}
+        filled={true}
+        lineWidthMinPixels={2}
+        opacity={0.4}
+        getLineColor={[60, 60, 60]}
+        getFillColor={[200, 200, 200]}
+      />
+      <GeoJsonLayer
+        id="airports"
+        data={AIR_PORTS}
+        filled={true}
+        pointRadiusMinPixels={2}
+        pointRadiusScale={2000}
+        getPointRadius={(f) => 11 - f.properties.scalerank}
+        getFillColor={[200, 0, 80, 180]}
+        pickable={true}
+        autoHighlight={true}
+        onClick={onClick}
+      />
+      <ArcLayer
+        id="arcs"
+        data={AIR_PORTS}
+        dataTransform={(d) =>
+          d.features.filter((f) => f.properties.scalerank < 4)
+        }
+        getSourcePosition={(f) => [-0.4531566, 51.4709959]}
+        getTargetPosition={(f) => f.geometry.coordinates}
+        getSourceColor={[0, 128, 200]}
+        getTargetColor={[200, 0, 80]}
+        getWidth={1}
+      />
+    </DeckGL>
+  );
 }
 
-export default App
+/* global document */
+const container = document.body.appendChild(document.createElement("div"));
+createRoot(container).render(<Root />);
